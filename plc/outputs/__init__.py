f444c8f903cd70b2de4fd0a5e7b126c661f72f9c
Petr Kracík <petrkr@petrkr.net>
2021-12-21 16:58:16 +0100

    Initial output commit

---
+++ plc/outputs/__init__.py
@@ -0,0 +1,32 @@
+from plc import PLCBase, PLCInterrupt
+
+
+class PLCOutput(PLCBase):
+    def __init__(self, input):
+        self._value = None
+        self._input = input
+        self._interrupts = []
+
+
+    def __interrupt__(self, direction, value):
+        for f in self._interrupts:
+            f(self, direction, value)
+
+
+    def add_interrupt(self, func):
+        self._interrupts.append(func)
+
+
+    def update(self):
+        tmp = self._input.output
+        if tmp == self._value:
+            return
+
+        direction = PLCInterrupt.RISING if tmp else PLCInterrupt.FALLING
+        self._value = tmp
+        self.__interrupt__(tmp, direction)
+
+
+    @property
+    def value(self):
+        return self._value
