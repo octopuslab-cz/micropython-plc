f384fd11e24c187c4aa6360b04e76056ed489a3a
Petr Kracík <petrkr@petrkr.net>
2021-12-21 15:30:44 +0100

    Sync from octopus

---
+++ plc/inputs/__init__.py
@@ -0,0 +1,11 @@
+from plc import PLC_base
+
+
+class PLC_input(PLC_base):
+    def __init__(self):
+        self._value = None
+        pass
+
+    @property
+    def output(self):
+        return self._value
---
+++ plc/operands/__init__.py
@@ -0,0 +1,13 @@
+from plc import PLC_base
+
+
+class PLC_operand(PLC_base):
+    def input(self):
+        raise NotImplementedError()
+
+    def inputs(self):
+        raise NotImplementedError()
+
+    @property
+    def output(self):
+        raise NotImplementedError()
