de901faed62b6d5fa76d3742d458509e93ce7bfc
Petr Kracík <petrkr@petrkr.net>
2021-12-21 21:41:22 +0100

    TestHW: added NOT operand

--- testhw.py
+++ testhw.py
@@ -1,6 +1,8 @@
 from machine import I2C, Pin
 from plc.inputs.virtual import PLCInputVirtual
+from plc.drivers.octopus_plcshield import PLCOctopusLabShield
 
+from plc.operands.op_not import PLCOperandNOT
 
 i2c = I2C(1, scl=Pin(16), sda=Pin(2), freq=100000)
 i2c.scan()
@@ -17,8 +19,8 @@ o1 = plc.outputs[0]
 
 plcin2 = plc.inputs[1]
 plcin2.add_interrupt(input_interrupt)
-o1.set_input(plcin2)
-
+nt = PLCOperandNOT(plcin2)
+o1.set_input(nt)
 
 while True:
     plcin2.read()
