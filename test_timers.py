3fb9d2c0760cd11866dcf44df0283c40956b552e
Petr Kracík <petrkr@petrkr.net>
2021-12-23 19:01:45 +0100

    Exapmle test timers

---
+++ test_timers.py
@@ -0,0 +1,33 @@
+from plc.operands.op_not import PLCOperandNOT
+from plc.inputs.virtual import PLCInputVirtual
+from plc.outputs.virtual import PLCOutputVirtual
+from plc.elements.rs import PLCElementRS
+from plc.elements.timers.ton import PLCTimerOn
+
+
+def testint(inp, value, direction):
+    print("Input {} changed to {} direction {}".format(inp, value, direction))
+
+
+rs1 = PLCElementRS()
+ton = PLCTimerOn(rs1, 2000, "Timer 1")
+
+i1 = PLCInputVirtual(False, "vI1")
+i2 = PLCInputVirtual(False, "vI2")
+o1 = PLCOutputVirtual(ton, "vO1")
+
+rs1.set = i1
+rs1.reset = i2
+
+from time import sleep
+for i in range(12):
+    print("Timer TT:{} EN:{} DN:{} Accum: {}".format(ton.active.output, ton.enabled.output, ton.output, ton.accum))
+    if i == 2:
+        i1.value = True
+        i1.value = False
+
+    if i == 8:
+        i2.value = True
+
+    
+    sleep(0.5)
