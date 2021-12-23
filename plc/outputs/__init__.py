2a592855cc83f123677faf359f03316e86e73482
Petr Kracík <petrkr@petrkr.net>
2021-12-23 13:11:43 +0100

    Output: events

--- plc/outputs/__init__.py
+++ plc/outputs/__init__.py
@@ -5,11 +5,12 @@ class PLCOutput(PLCBase):
     def __init__(self, input=None, name=None):
         super().__init__(name)
         self._value = None
+        input.add_event_on_change(self.__on_input_change)
         self._input = input
 
 
-    def set_input(self, input):
-        self._input = input
+    def __on_input_change(self, input, value, dir):
+        self.update()
 
 
     def update(self):
