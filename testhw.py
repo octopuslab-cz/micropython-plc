6909854e29b51dbc68079dec1b8a2a8bab0bafe3
Petr Kracík <petrkr@petrkr.net>
2021-12-23 23:03:23 +0100

    TestHW: test with RS

--- testhw.py
+++ testhw.py
@@ -1,30 +1,53 @@
 from machine import I2C, Pin
-from plc.inputs.virtual import PLCInputVirtual
 from plc.drivers.octopus_plcshield import PLCOctopusLabShield
+
+from plc.elements.rs import PLCElementRS
+from plc.elements.timers.ton import PLCTimerOn
 
 from plc.operands.op_not import PLCOperandNOT
 
+print("Init I2C")
 i2c = I2C(1, scl=Pin(16), sda=Pin(2), freq=100000)
 i2c.scan()
 
-def on_value_change(cls, value, direction):
-    print("Handle change on {} to {}".format(cls,value))
-
+print("Init PLC")
 plc = PLCOctopusLabShield(i2c)
 
-i1 = PLCInputVirtual(False)
-
+print("Init PLC Inputs")
 plc_i2 = plc.inputs[1]
+plc_i3 = plc.inputs[2]
+
+
+print(("Init PLC Outputs"))
 plc_o1 = plc.outputs[0]
+plc_o2 = plc.outputs[1]
+plc_o3 = plc.outputs[2]
+plc_o4 = plc.outputs[3]
 
-plc_i2.add_event_on_change(on_value_change)
-plc_o1.add_event_on_change(on_value_change)
-
+# Simple NOTPL
+#  Input 2
+#  Output 1
 nt = PLCOperandNOT(plc_i2)
-nt.add_event_on_change(on_value_change)
+plc_o1.input = nt
 
-plc_o1.set_input(nt)
+# Test timer 2 sec Power On Delay
+#  Input 2
+#  Output 2 timer Done bit (output)
+#  Output 3 timer Enable bit
+#  Output 4 timer Active bit
+rs1 = PLCElementRS(plc_i2, plc_i3)
+ton = PLCTimerOn(rs1, 2000, "TON1")
 
+ton_nt_dn = PLCOperandNOT(ton, "NOT: DN")
+plc_o2.input = ton_nt_dn
+
+ton_nt_en = PLCOperandNOT(ton.enabled, "NOT: EN")
+plc_o3.input = ton_nt_en
+
+ton_nt_tt = PLCOperandNOT(ton.activated, "NOT: TT")
+plc_o4.input = ton_nt_tt
 
 while True:
     plc_i2.read()
+    plc_i3.read()
+    ton.loop()
